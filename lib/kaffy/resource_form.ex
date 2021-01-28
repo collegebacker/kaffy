54a58429da7397ad74b1d35b840709bfa4d3ffe5
Mathieu Lajugie <mathieu@collegebacker.com>
2021-01-28 11:37:44 -0800

    enforce readonly for booleans

--- lib/kaffy/resource_form.ex
+++ lib/kaffy/resource_form.ex
@@ -154,7 +154,7 @@ defmodule Kaffy.ResourceForm do
         text_input(form, field, opts)
 
       t when t in [:boolean, :boolean_checkbox] ->
-        checkbox_opts = Keyword.put(opts, :class, "custom-control-input")
+        checkbox_opts = checkbox_options(opts)
         label_opts = Keyword.put(opts, :class, "custom-control-label")
 
         [
@@ -165,7 +165,7 @@ defmodule Kaffy.ResourceForm do
         ]
 
       :boolean_switch ->
-        checkbox_opts = Keyword.put(opts, :class, "custom-control-input")
+        checkbox_opts = checkbox_options(opts)
         label_opts = Keyword.put(opts, :class, "custom-control-label")
 
         [
@@ -240,6 +240,16 @@ defmodule Kaffy.ResourceForm do
 
       _ ->
         text_input(form, field, opts)
+    end
+  end
+
+  defp checkbox_options(opts) do
+    checkbox_opts = Keyword.put(opts, :class, "custom-control-input")
+
+    if opts[:readonly] do
+      Keyword.put(checkbox_opts, :disabled, true)
+    else
+      checkbox_opts
     end
   end
 
