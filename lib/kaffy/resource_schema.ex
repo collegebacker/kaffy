09374a309f6503ec214e6bbbd1222fc8c286c305
Mathieu Lajugie <mathieu@collegebacker.com>
2021-01-27 18:13:07 -0800

    update target_url function arity

--- lib/kaffy/resource_schema.ex
+++ lib/kaffy/resource_schema.ex
@@ -221,8 +221,8 @@ defmodule Kaffy.ResourceSchema do
   def kaffy_field_sortable?(_schema, {_field, options}), do: !Map.get(options || %{}, :no_sort, false)
   def kaffy_field_sortable?(_schema, _field_options), do: false
 
-  def kaffy_target_url(entry, context, {field, %{target_url: target_url}}) when is_function(target_url) do
-    target_url.(entry, field, context)
+  def kaffy_target_url(entry, context, {_field, %{target_url: target_url}}) when is_function(target_url) do
+    target_url.(entry, context)
   end
 
   def kaffy_target_url(entry, _context, {_field, %{target_url: target_url}}) when is_binary(target_url) do
